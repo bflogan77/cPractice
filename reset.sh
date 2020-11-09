3db208423308d1006019fc8612779f9d7996e84a
Brock Logan <brock.f.logan@gmail.com>
2020-11-08 18:47:33 -0600

    functional

--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo -e "3.0 4.0\n5.0 6.0\n" > aAndB.txt
+echo -e "3.0 4.0\n5.0 6.0" > aAndB.txt
--- week1.c
+++ week1.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     double a;
     double b;
+    double c;
     int numLines;
     char fName[]="aAndB.txt";
     FILE * fp;
@@ -17,23 +18,37 @@ int main(int argc, char *argv[])
     //scanf("%f", &b);
     numLines = linesInFile(fName);
     fp = fopen (fName, "r");
-    double c[numLines];
+    double outArray[numLines][3];
     int i;
+    int j;
     char fChar;
     for (i=0; i<numLines; i=i+1){//read a and b, calculate c and store in c[] array
         fscanf(fp, "%lf", &a);
+        outArray[i][0] = a;
         fscanf(fp, "%lf", &b);
-        c[i]=hyp(a, b);
+        outArray[i][1] = b;
+        outArray[i][2] =hyp(a, b);
     }
     fclose(fp);
     fp = fopen (fName, "w");
     for (i=0; i<numLines; i=i+1){//append c contents to each file line
-        for (fChar=getc(fp); fChar!=EOF; fChar=getc(fp)){ //scan to the end of each line
-            if (fChar=="\n"){
+        
+        for (j=0; j<3; j=j+1){
+            if (j<2){
+                fprintf(fp, "%.2lf ", outArray[i][j]);
+            }
+            else {
+                fprintf(fp, "%.2lf\n", outArray[i][j]);
+            }
+        }
+        /*
+        while ((fChar=fgetc(fp))!=EOF){//scan to the end of each line
+            if (fChar==10){ //if at newline char
                 //append here
                 fprintf(fp, "%f", c[i]);
             }
         }
+        */
     }
     // fprintf(fp, "%f\n", a);
     // fprintf(fp, "%f\n", b);
@@ -50,8 +65,8 @@ int linesInFile(char fName[]){
         printf("Could not open file: %s", fName);
         return 0;
     }
-    for (c=getc(fp); c!=EOF; c=getc(fp)){
-        if ((char)c=="\n"){
+    while ((c=fgetc(fp))!=EOF){
+        if ((char)c==10){ //look for newline char
             count += 1;
         }
     }
